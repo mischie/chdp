0d4e3ff57b0b75d87f1699390118a4574b348969
kiki7000 <51841836+kiki7000@users.noreply.github.com>
2020-02-18 23:40:45 +0900

    Update command.py

--- command.py
+++ command.py
@@ -8,7 +8,7 @@ async def cmdrun(client, message, prefix):
         if filename.endswith('.py'):
             imported = filename.split('.py')[0]
             mod = importlib.import_module(f'commands.{imported}')
-            x = mod.name()
-            y = mod.aliases()
+            x = mod.name
+            y = mod.aliases
             if x == cmd or cmd in y:
                 await mod.run(client, message, args)
