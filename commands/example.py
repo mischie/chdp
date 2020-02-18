2c2db697213d7f6d275eb2d2ed8745483ea569d9
kiki7000 <51841836+kiki7000@users.noreply.github.com>
2020-02-18 23:41:28 +0900

    Update example.py

--- commands/example.py
+++ commands/example.py
@@ -1,8 +1,5 @@
-def name():
-    return 'example'
-
-def aliases():
-    return ['example1', 'example2']
+name = 'example'
+aliases = ['example1', 'example2']
 
 async def run(client, message, args):
     await message.channel.send('CHDP is Great')
