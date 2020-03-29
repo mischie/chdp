956e341dad67291b5d6c06c08af86ba05db93c87
kiki7000 <51841836+kiki7000@users.noreply.github.com>
2020-03-29 14:17:23 +0900

    Update v2.0

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import discord
-import os
-import command
+import funcs
+
+cmds = funcs.get_cmds()
 
 client = discord.Client()
 token = 'Your Bot\'s Token'
@@ -10,15 +11,24 @@ game = 'Your Bot\'s Game'
 @client.event
 async def on_ready():
     print('The Bot is ready!')
-    await client.change_presence(status=discord.Status.online  , activity=discord.Game(game))
+    await client.change_presence(status = discord.Status.online  , activity = discord.Game(game))
 
 @client.event
 async def on_message(message):
     if message.channel.type == 'private': return
     if message.author.bot: return
     if message.content.startswith(prefix) == False: return
-
-    await command.cmdrun(client, message, prefix)
-
+    
+    cmd = message.content.split(prefix)[1].split(' ')[0]
+    args =message.content.split(cmd)[1][1:].split(' ')
+    
+    for c in cmds:
+        if cmd in c[0]:
+            try:
+                c[1].run(client, message, args)
+            except Excetipon as e:
+                print('Error: ' + str(e))
+            
+            break
 
 client.run(token)   
