35e9e3ac7dada795a774c12efab1d763bf809909
kiki7000 <51841836+kiki7000@users.noreply.github.com>
2020-03-29 14:14:00 +0900

    Updated v2.0
    Added File

---
+++ funcs.py
@@ -0,0 +1,15 @@
+import importlib
+import os
+import commands as cmds
+
+def import_module(module):
+    return importlib.import_module(str(module))
+
+def get_listdir(dir):
+    return os.listdir(dir)
+
+def remove_end(name):
+    return str(name.split('.')[0])
+
+def get_cmds():
+    return cmds.get_cmds()
