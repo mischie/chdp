ae0d269ceedda63038edd186f38d62009aa0086a
kiki7000 <51841836+kiki7000@users.noreply.github.com>
2020-08-10 19:12:58 +0900

    Delete index.py

--- example/index.py
+++
@@ -1,9 +0,0 @@
-import client as chdpclient
-
-client = chdpclient.CHDPClient(prefix = '!')
-
-@client.event
-async def on_message(message):
-  await client.use_cmd(message)
-
-client.run('키키키키키키(이거 토큰임 오해하지 마3')
